efee2faf29820cb0ac43565b7db24976fa2fe1a3
mrrmka <mariakarkosko@gmail.com>
2023-12-25 15:07:11 +0300

    add 13 lab

--- labs/Lab_13_python_oop/solution/base.py
+++ labs/Lab_13_python_oop/solution/base.py
@@ -1,12 +1,10 @@
 from abc import ABC, abstractmethod
 
-
 class BaseXlsBlock(ABC):
+    ROW_INCREMENT = 12
+
     def __init__(self, worksheet, row, col, data):
-        self.worksheet = worksheet
-        self.row = row
-        self.col = col
-        self.data = data
+        self.worksheet, self.row, self.col, self.data = worksheet, row, col, data
 
     @abstractmethod
     def write_header(self):
@@ -15,3 +13,6 @@ class BaseXlsBlock(ABC):
     @abstractmethod
     def write_data(self):
         pass
+
+    def get_row_increment(self):
+        return self.ROW_INCREMENT
--- labs/Lab_13_python_oop/solution/main.py
+++ labs/Lab_13_python_oop/solution/main.py
@@ -3,26 +3,31 @@ import json
 import os
 from writer import XlsAnalyticPaymentWriter
 
+from datetime import datetime
+import json
+import os
+from writer import Writer
+
 def load_data(file_path):
-    full_path = os.path.join(
-        "C:\\Users\\THRIKE\\Desktop\\git\\web_lab_aib_frontend\\labs\\Lab_13_python_oop",
-        file_path
-    )
-
+    full_path = os.path.join(os.getcwd(), file_path)
     with open(full_path, 'r', encoding='utf-8') as file:
-        data = json.load(file)
-    return data
+        return json.load(file)
 
-if __name__ == '__main__':
-    data_clients = load_data('clients.json')
-    data_payments = load_data('payments.json')
-
-    data = {'clients': data_clients['clients'], 'payments': data_payments['payments']}
-
+def generate_report_filename():
     timestamp = datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
-    output_file = f'my_payments_analytics_{timestamp}.xlsx'
+    return os.path.join(os.getcwd(), f'my_payments_analytics_{timestamp}.xlsx')
 
-    xls_writer = XlsAnalyticPaymentWriter(data)
+def main():
+    clients_data = load_data('clients.json')['clients']
+    payments_data = load_data('payments.json')['payments']
+
+    data = {'clients': clients_data, 'payments': payments_data}
+    output_file = generate_report_filename()
+
+    xls_writer = Writer(data)
     xls_writer.write_excel_report(output_file)
 
     print(f"Report generated successfully. Output file: {output_file}")
+
+if __name__ == '__main__':
+    main() Output file: {output_file}")
